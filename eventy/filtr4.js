994bef34ef658e78b9631d6389811c5601415a15
swagcat7889 <pieprzyc.dejwidka@gmail.com>
2021-12-31 11:28:54 +0100

    Próba naprawienia filtru 4 (nitro scam)

--- eventy/filtr4.js
+++ eventy/filtr4.js
@@ -2,8 +2,7 @@ module.exports = {
     "eventName": "messageCreate",
     once: false,
     execute(message) {
-        // if(message.member.roles.cache.some(role => role.id === '818452928909279272')) return;
-        if(!message.member.roles.cache.some((role) => role.id === '8834446487403364392')) return;
+        try {if(!message.member.roles.cache.some((role) => role.id === '8834446487403364392')) return;} catch (err) {}
         if (message.content.includes('.ru/id') || message.content.includes('@everyone') || message.content.includes('discord nitro') || message.content.includes('cutt.ly') || message.content.includes('bit.ly')) {
             let role = message.member.guild.roles.cache.find(role => role.id === "821698344728002562");
             if (role) message.guild.members.cache.get(message.author.id).roles.remove(role);
