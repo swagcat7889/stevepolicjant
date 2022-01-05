4168aaf0a0e6f38da8a7d43c1ad37457c80d609e
swagcat7889 <pieprzyc.dejwidka@gmail.com>
2022-01-05 10:10:22 +0100

    OK

--- komendy/snipe.js
+++ komendy/snipe.js
@@ -5,7 +5,7 @@ module.exports = {
         const cache = await global.keyv.get(message.channel.id);
         if (cache === undefined || cache === null) return message.reply('404 brak snipe');
         const embed = new MessageEmbed().
-        setTitle(message.author.id).
+        setTitle(message.author.tag).
         setDescription(`Ostatnia usunięta wiadomość z kanału <#${message.channel.id}>\nAutor wiadomości: **${cache.author.tag}**\n\n**Wiadomość**\n\`${cache.content.replaceAll('`', '')}\``);
         return message.reply({ embeds: [embed] }).catch((err) => process.stderr.write(`${err.name}: ${err.message}\n`));
     }
